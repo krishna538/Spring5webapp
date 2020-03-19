7a6ca752a2ebec0d685fe86c938dfde876d2023a
Ramya <kramya8912@email.com>
2020-03-19 23:58:00 +0530

    first commit

---
+++ src/test/java/krish/springframework/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package krish.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
